10579df3241a0f5618b90ff1e49f154834e3779f
Sonia <soniakeys@gmail.com>
2018-01-28 22:53:34 -0500

    4.1 Producer-consumer

---
+++ 4-Classical_Problems/4.1-Producer-consumer/chanSem.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"sync/atomic"
+
+	"github.com/soniakeys/LittleBookOfSemaphores/sem"
+)
+
+func producer(np int) {
+	event := WaitForEvent()
+	log.Println("producer", np, "produces event", event)
+	mutex.Wait()
+	buffer.add(event)
+	mutex.Signal()
+	items.Signal()
+}
+
+func consumer(nc int) {
+	for {
+		items.Wait()
+		mutex.Wait()
+		event := buffer.get()
+		mutex.Signal()
+		log.Println("  consumer", nc, "gets event", event)
+		event.process()
+	}
+}
+
+var (
+	last   int64 // last event number
+	mutex  sem.ChanSem
+	items  sem.ChanSem
+	buffer eventBuffer
+	wg     sync.WaitGroup
+)
+
+type event64 int64
+
+func (e event64) process() {
+	log.Println("    processed: event", e)
+	wg.Done()
+}
+
+func WaitForEvent() event64 {
+	return event64(atomic.AddInt64(&last, 1))
+}
+
+type eventBuffer []event64
+
+func (b *eventBuffer) add(e event64) {
+	*b = append(*b, e)
+}
+
+func (b *eventBuffer) get() (e event64) {
+	e = (*b)[0]
+	*b = (*b)[1:]
+	return
+}
+
+const nEvents = 6
+
+func main() {
+	mutex = sem.NewChanSem(1, 1)
+	items = sem.NewChanSem(0, nEvents)
+	go consumer(1)
+	go consumer(2)
+	wg.Add(nEvents)
+	for i := 1; i <= nEvents; i++ {
+		go producer(i)
+	}
+	wg.Wait()
+}
